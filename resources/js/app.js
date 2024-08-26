bb45cf660ee9e8093f072db3c0e3ae8517defe17
Ivangrb10 <ivanrevelo1992@gmail.com>
2024-08-25 19:23:03 -0500

    se instala datatable

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-import './bootstrap';
+import $ from 'jquery';
+import 'datatables.net';
+import 'datatables.net-bs4';
+
+// Inicializa DataTables cuando el documento esté listo
+$(document).ready(function() {
+    $('#categorias').DataTable();
+});
--- resources/views/categorias/index.blade.php
+++ resources/views/categorias/index.blade.php
@@ -1,17 +1,9 @@
 @extends('layouts.app')
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mi Proyecto Laravel</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <!-- Otros enlaces a CSS o scripts -->
-</head>
-<body>
-    @yield('content')
-</body>
-</html>
+
+@section('css')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.1.4/css/dataTables.bootstrap5.css">
+@endsection
 
 @section('content')
 <div class="container">
@@ -20,7 +12,7 @@
             Categorías
         </div>
         <div class="card-body">
-            <table class="table table-striped table-hover">
+            <table class="table table-striped table-hover" id="categorias">
                 <thead>
                     <tr>
                         <th>#</th>
@@ -62,3 +54,14 @@
     </div>
 </div>
 @endsection
+
+@section('js')
+    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.datatables.net/2.1.4/js/dataTables.js"></script>
+    <script src="https://cdn.datatables.net/2.1.4/js/dataTables.bootstrap5.js"></script>
+    <script>
+        new DataTable('#categorias');
+    </script>
+@endsection
+
